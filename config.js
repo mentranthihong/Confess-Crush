73af85858d2d3b2022c1004d12f8250bcb3adfdb
mentranthihong <96191967+mentranthihong@users.noreply.github.com>
2021-12-25 21:57:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    titleWeb: "Nhóm 1",
+    introTitle: 'Hi các bạn',
+    introDesc: `Nhân dịp Noel 
+    Chúc các bạn có một giáng sinh an lành
+    Chúc những ai chưa có gấu thì sẽ có gấu
+    Ai có gấu rồi thì miễn phát cơmtros nhó`,
+    btnIntro: '^^AHiHi^^',
+    title: 'Bạn có muốn nhận quà Noel không nàooooo 🥰',
+    desc: 'Hãy chọn cho mình một món quà nhó ',
+    btnYes: '+1 tình iu thương từ Đội Toán <33',
+    btnNo: '500k :3',
+    question: 'Bạn có thích món quà này không nhỉ <3',
+    btnReply: 'Mình thích lắm nhé <3',
+    reply: 'Yêu ká <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Bạn muốn nhận thêm quà nữa không nè.',
+    btnAccept: 'Tất nhiên rồi <3',
+    messLink: 'https://drive.google.com/file/d/1FObhFolMvdI_CxmvABsrUvOKwpCWHhwV/view?fbclid=IwAR1qsOcSDPu4PV_NXqAuLRTiyvTCOomX3mDho-JS91A_3GlXGIc5KajIBFQ' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
