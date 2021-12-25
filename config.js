bec8f127a28e0b1bdb44935f6ca9ce6b79e59e48
mentranthihong <96191967+mentranthihong@users.noreply.github.com>
2021-12-25 10:58:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     titleWeb: "Nhận quà Giáng sinh ",
     introTitle: 'Mình là Mến Bri chăm chỉ nhất Ban TCKT đây',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Mến Bri chúc đồng chí có một giáng sinh vui vẻ,
+    một tháng cuối năm gặp nhiều may mắn nha <3
+    Đồng chí nào chưa có người yêu thì sớm có 1 bé nhé,
+    Đồng chí nào có rồi thì sẽ có nhiều deadline để hông phát được cơmtros nhó`,
     btnIntro: '^^HiHi^^',
     title: 'Hãy chọn cho mình một phần quà đón Noel nàoooooo 🥰',
     desc: ' ',
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu đồng chí nhiều nhiều 😘😘',
     messDesc: 'Đồng chí muốn nhận thêm món quà không :>>',
     btnAccept: 'Tất nhiên rồi <3',
-    messLink: 'https://drive.google.com/drive/my-drive?fbclid=IwAR3iJ5Bz0YufnLa6XYb1scG2ZMiY2rr0k5TRe9dbv1HmZxZeoSK-eR0Dzvc' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://drive.google.com/file/d/1UA4wdLfhYRX6EXY_rny7B6CQeabrTnpv/view' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
